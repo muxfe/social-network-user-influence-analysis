d1843307948730ce18cf396c3a11a549b3eaee06
kyer <grzpjdyc1653@126.com>
2016-05-05 21:56:05 +0800

    update aminer crawler

--- aminer/cralwer.py
+++ aminer/cralwer.py
@@ -24,20 +24,26 @@ def fetchUser(start = 0, offset = 99, limit = 100, sleeptime = 3):
     fwriter.close()
     return
 
-def fetchPub(uid, start = 0, offset = 99, limit = 100, sleeptime = 3):
+def fetchPub(sleeptime = 3):
+    freader = open('user.json', 'r')
     fwriter = open('publication.json', 'w')
-    api = 'https://api.aminer.org/api/person/pubs/' + uid +'/all/year/'
-    while (start < limit):
-        curapi = api + str(start) + '/' + str(offset)
-        print 'fetch ' + curapi
-        response = urllib2.urlopen(urllib2.Request(curapi))
-        data = response.read()
-        fwriter.write(data + "\n")
-        start = start + offset + 1
-        time.sleep(sleeptime)
+
+    for raw_data in freader:
+        json_array = json.loads(raw_data)
+        for user in json_array:
+            uid = user['id']
+            n_pubs = user['n_pubs']
+            api = 'https://api.aminer.org/api/person/pubs/' + str(uid) +'/all/year/0/' + str(n_pubs)
+            print 'fetch ' + api
+            response = urllib2.urlopen(urllib2.Request(api))
+            data = response.read()
+            fwriter.write(data + "\n")
+            time.sleep(sleeptime)
+    freader.close()
     fwriter.close()
     return
 
 if __name__ == '__main__':
-    fetchUser(limit = 10000)
+    # fetchUser(limit = 10000)
+    fetchPub()
     print 'Done!'
