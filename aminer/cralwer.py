ffc4dcb923afac501e20639ac642f7c2c5f7b414
kyer <grzpjdyc1653@126.com>
2016-05-11 20:17:23 +0800

    chap1

--- aminer/cralwer.py
+++ aminer/cralwer.py
@@ -13,31 +13,49 @@ import time
 def fetchUser(start = 0, offset = 99, limit = 100, sleeptime = 3):
     fwriter = open('user.json', 'w')
     api = 'https://api.aminer.org/api/rank/person/h_index/'
+    errortime = 0
+    count = 1
     while (start < limit):
         curapi = api + str(start) + '/' + str(offset)
         print 'fetch ' + curapi
-        response = urllib2.urlopen(urllib2.Request(curapi))
-        data = response.read()
-        fwriter.write(data + "\n")
-        start = start + offset + 1
+        try:
+            response = urllib2.urlopen(urllib2.Request(curapi))
+            data = response.read()
+            fwriter.write(data + "\n")
+            start = start + offset + 1
+            print str(count) + ' ok!'
+        except:
+            print str(count) + ' error!'
+            errortime += 1
+            if errortime > 3:
+                start = start + offset + 1
         time.sleep(sleeptime)
+        count += 1
     fwriter.close()
     return
 
-def fetchPub(sleeptime = 3):
+def fetchPub(sleeptime = 3, pass = 0):
     freader = open('user.json', 'r')
     fwriter = open('publication.json', 'w')
-
+    count = 0
     for raw_data in freader:
         json_array = json.loads(raw_data)
         for user in json_array:
+            count += 1
+            if count <= pass:
+                print 'pass ' + str(count)
+                continue
             uid = user['id']
             n_pubs = user['n_pubs']
             api = 'https://api.aminer.org/api/person/pubs/' + str(uid) +'/all/year/0/' + str(n_pubs)
             print 'fetch ' + api
-            response = urllib2.urlopen(urllib2.Request(api))
-            data = response.read()
-            fwriter.write(data + "\n")
+            try:
+                response = urllib2.urlopen(urllib2.Request(api))
+                data = response.read()
+                fwriter.write(data + "\n")
+                print str(count) + ' ok!'
+            except:
+                print str(count) + ' error!'
             time.sleep(sleeptime)
     freader.close()
     fwriter.close()
